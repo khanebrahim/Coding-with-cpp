2c03607f546d4c859ae70a987208bd6e8e492e34
BengalDroid <bengaldroid@gmail.com>
2021-07-16 12:13:42 +0600

    addition

---
+++ intwonumber.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"Enter two numbers:"<<endl;
+    cin >>a>>b;
+    c=a+b;
+    cout<<"sum is = "<<c<<endl;
+  
+    return 0;
+    
+}
