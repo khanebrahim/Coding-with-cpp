f296fe8c4200cca899adc255597f5256ab23f787
BengalDroid <bengaldroid@gmail.com>
2021-07-16 11:47:51 +0600

    first commit

---
+++ username.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string name;
+    cout<<"May I know your name:";
+    cin >> name;
+    cout<<"Welcome MR.and MISS "<<name<<endl;
+    return 0;
+}
