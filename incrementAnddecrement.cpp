1e959242afa68998b07308fe02dd1bba761a92b2
BengalDroid <bengaldroid@gmail.com>
2021-07-17 22:57:21 +0600

    overflow

--- incrementAnddecrement.cpp
+++ incrementAnddecrement.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     int i=5,j;
-    j=2*i++ +2 *i++;
+    j=2* ++i + 2 * ++i;
     cout<<i<<" "<<j<<endl;
     return 0;
 
---
+++ overflow.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char x=128;
+    cout<<(int)x<<endl;
+    return 0;
+}
