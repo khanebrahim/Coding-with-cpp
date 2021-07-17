4a50e45e603b0357870ddf5cd044272c4b0d430a
BengalDroid <bengaldroid@gmail.com>
2021-07-17 22:26:11 +0600

    post increment

--- compound-assignment.cpp
+++ compound-assignment.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     int sum =10;
     int x=5;
-    sum +=x;
+    sum *=x;
     cout<<sum;
     return 0;
 }
---
+++ incrementAnddecrement.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i=5,j;
+    j=2*i++ +2 *i++;
+    cout<<i<<" "<<j<<endl;
+    return 0;
+
+
+}
