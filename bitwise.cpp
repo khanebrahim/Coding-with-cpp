fcd6b6a2bff609c36136255693503f180cd62bd6
BengalDroid <bengaldroid@gmail.com>
2021-07-18 15:09:43 +0600

    master

--- bitwise.cpp
+++ bitwise.cpp
@@ -6,6 +6,7 @@ int main(){
 
     cout<<(int)y<<endl;
     
+    
     return 0;
 
 }
---
+++ conditional-statement.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int roll;
+    cout<<"Enter your roll number:"<<endl;
+    cin >> roll;
+    if(roll<1){
+        cout<<"Roll number is ivalid"<<endl;
+
+    }
+    else{
+        cout<<"Roll number is valid"<<endl;
+    }
+    return 0;
+}
